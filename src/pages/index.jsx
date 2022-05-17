176418eca500212208cd99f8346ce1fe3e36da04
alexz330 <alexislc330@gmail.com>
2022-05-17 01:03:05 -0600

    bug fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import SideBar from "../Components/SideBar/Index";
 import Home from "../Containers/Home/Index";
 import styles from "../styles/index.module.css";
-import Skills from "../Containers/Skills";
+import Skills from "../Containers/Skills/Index";
 import TabBar from "../Components/TabBar/Index";
 
 export default function Index() {
